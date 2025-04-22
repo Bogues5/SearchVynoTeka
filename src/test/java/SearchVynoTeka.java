bb57741980b17784e8431ae6fca0f12a390a1073
Bogues5 <suverenas@tutanota.com>
2025-04-22 12:03:46 +0300

    Tikriname kaip atidaro Vynoteka puslapį

--- src/test/java/SearchVynoTeka.java
+++ src/test/java/SearchVynoTeka.java
@@ -1,8 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class SearchVynoTeka {
     @Test
     public void SearchTypeClick() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        // Nustatome laukimo laika
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+        driver.get("https://Vynoteka.lt");
+
+
 
 
 
