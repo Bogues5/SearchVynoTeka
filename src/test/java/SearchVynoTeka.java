b664399c22bb2c13e982ddc2f9b0f9490ec10e8d
Bogues5 <suverenas@tutanota.com>
2025-04-22 16:48:19 +0300

    Tikriname kaip atidaro Vynoteka puslapį. Atidaro pirmiausia amžiaus patikrinima po to slapukus o po to pop-up reklama pataisytas.

--- src/test/java/SearchVynoTeka.java
+++ src/test/java/SearchVynoTeka.java
@@ -17,20 +17,30 @@ public class SearchVynoTeka {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
         driver.get("https://www.vynoteka.lt");
 
         WebElement acceptBtn = driver.findElement(By.className("c-button--blue"));
         acceptBtn.click();
+        driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[2]/div/div/div/div/div[2]/div[3]/div/div[1]/button")).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[4]/div[1]/div/div[2]/div[2]/div[2]/button/div"))).click();
+
+
 
         driver.findElement(className("has-icon--left")).sendKeys("Degtinė");
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.className("search-form__content search-form__content--desktop")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("search-form__content--desktop")));
 
 
         wait.until(ExpectedConditions.elementToBeClickable(cssSelector("button[type='submit']")));
         driver.findElement(cssSelector("button[type='submit']")).click();
+
+
+
+
     }
 }
 
